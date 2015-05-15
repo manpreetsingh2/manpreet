c71e18dd949937c3cbd04268bbf737fe52d9c740
ManpreetSingh <ManpreetSingh@Manpreet>
2015-05-14 20:52:01 -0500

    Update

---
+++ ManPreetCoreJava/src/ArraysExample.java
@@ -0,0 +1,25 @@
+
+public class ArraysExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int age1 = 10;
+		int age2 = 20;
+		int age3 = 30;
+		
+		/*String name ;
+		name = "Jayram";*/
+		
+		int ages[] = new int[3]; //{10,20,30};
+		ages[0] = age1;
+		ages[1] = age2;
+		ages[2] = age3;
+		
+//		ages[3] = 40;
+
+		System.out.println(" Length is "+ages.length);
+
+	}
+
+}
