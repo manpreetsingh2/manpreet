45d2f0772a2738a358ac10b6e42279ee5128202e
ManpreetSingh <ManpreetSingh@Manpreet>
2015-05-16 21:11:11 -0500

    Update

---
+++ ManPreetCoreJava/src/basic/HelloWorld.java
@@ -0,0 +1,11 @@
+package basic;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+			System.out.println("Hello World");
+			System.out.println("Hello World".toUpperCase());
+			System.out.println("Hello World".length());
+	}
+
+}
